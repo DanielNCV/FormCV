5883e7cd665845c8034efe8afe90e753490595ed
DanielNCV <113204281+DanielNCV@users.noreply.github.com>
2023-11-28 04:12:50 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormCV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string nombre= paraNombre.Text;
+            string apellido = paraApellido.Text;
+            string email = paraEmail.Text;
+            string dni = paraDNI.Text;
+
+            await Application.Current.MainPage.Navigation.PushAsync(new Page1(nombre, apellido, email, dni));
+        }
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormCV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(string nombre, string apellido, string email, string dni)
+        {
+            InitializeComponent();
+            nom.Text = nombre;
+            ape.Text = apellido;
+            em.Text = email;
+            d.Text = dni;
+        }
+    }
+}
